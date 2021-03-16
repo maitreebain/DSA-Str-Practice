e6397d923e9da532dfeea24da9fa8951aa6ca28a
maitreebain <maitreebain@gmail.com>
2021-03-15 23:53:04 -0400

    added str question

---
+++ Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+
+/*
+ #include <bits/stdc++.h>
+
+ using namespace std;
+
+ // Complete the marsExploration function below.
+ int marsExploration(string s) {
+
+
+ }
+
+ int main()
+ {
+     ofstream fout(getenv("OUTPUT_PATH"));
+
+     string s;
+     getline(cin, s);
+
+     int result = marsExploration(s);
+
+     fout << result << "\n";
+
+     fout.close();
+
+     return 0;
+ }
+
+ */
+
